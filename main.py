80ac1b21d7e1be16a16047f8fe37835dd56c86a9
Mikael Tulldahl <mikael.tulldahl@live.se>
2024-05-02 01:55:17 +0200

    Web-based animation editor

---
+++ main.py
@@ -0,0 +1,226 @@
+import asyncio
+import sys
+import traceback
+
+from js import THREE, Math, Object, performance
+from pyodide.ffi import to_js
+from pyscript import document, when, window
+
+mouse = THREE.Vector2.new()
+
+renderer = THREE.WebGLRenderer.new({"antialias": True})
+renderer.setSize(1000, 1000)
+renderer.shadowMap.enabled = False
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+renderer.shadowMap.needsUpdate = True
+
+document.body.appendChild(renderer.domElement)
+
+
+@when("mousemove", "body")
+def onMouseMove(event):
+    event.preventDefault()
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1
+
+
+camera = THREE.PerspectiveCamera.new(35, window.innerWidth / window.innerHeight, 1, 500)
+scene = THREE.Scene.new()
+cameraRange = 3
+
+camera.aspect = window.innerWidth / window.innerHeight
+camera.updateProjectionMatrix()
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+setcolor = "#000000"
+
+scene.background = THREE.Color.new(setcolor)
+scene.fog = THREE.Fog.new(setcolor, 2.5, 3.5)
+
+sceneGroup = THREE.Object3D.new()
+particularGroup = THREE.Object3D.new()
+
+
+def mathRandom(num=1):
+    setNumber = -Math.random() * num + Math.random() * num
+    return setNumber
+
+
+particularGroup = THREE.Object3D.new()
+modularGroup = THREE.Object3D.new()
+
+perms = {
+    "flatShading": True,
+    "color": "#111111",
+    "transparent": False,
+    "opacity": 1,
+    "wireframe": False,
+}
+perms = Object.fromEntries(to_js(perms))
+
+particle_perms = {"color": "#FFFFFF", "side": THREE.DoubleSide}
+particle_perms = Object.fromEntries(to_js(particle_perms))
+
+
+def create_cubes(mathRandom, modularGroup):
+    i = 0
+    while i < 30:
+        geometry = THREE.IcosahedronGeometry.new()
+        material = THREE.MeshStandardMaterial.new(perms)
+        cube = THREE.Mesh.new(geometry, material)
+        cube.speedRotation = Math.random() * 0.1
+        cube.positionX = mathRandom()
+        cube.positionY = mathRandom()
+        cube.positionZ = mathRandom()
+        cube.castShadow = True
+        cube.receiveShadow = True
+        newScaleValue = mathRandom(0.3)
+        cube.scale.set(newScaleValue, newScaleValue, newScaleValue)
+        cube.rotation.x = mathRandom(180 * Math.PI / 180)
+        cube.rotation.y = mathRandom(180 * Math.PI / 180)
+        cube.rotation.z = mathRandom(180 * Math.PI / 180)
+        cube.position.set(cube.positionX, cube.positionY, cube.positionZ)
+        modularGroup.add(cube)
+        i += 1
+
+
+create_cubes(mathRandom, modularGroup)
+
+
+def generateParticle(mathRandom, particularGroup, num, amp=2):
+    gmaterial = THREE.MeshPhysicalMaterial.new(particle_perms)
+    gparticular = THREE.CircleGeometry.new(0.2, 5)
+    i = 0
+    while i < num:
+        pscale = 0.001 + Math.abs(mathRandom(0.03))
+        particular = THREE.Mesh.new(gparticular, gmaterial)
+        particular.position.set(mathRandom(amp), mathRandom(amp), mathRandom(amp))
+        particular.rotation.set(mathRandom(), mathRandom(), mathRandom())
+        particular.scale.set(pscale, pscale, pscale)
+        particular.speedValue = mathRandom(1)
+        particularGroup.add(particular)
+        i += 1
+
+
+generateParticle(mathRandom, particularGroup, 200, 2)
+
+sceneGroup.add(particularGroup)
+scene.add(modularGroup)
+scene.add(sceneGroup)
+
+camera.position.set(0, 0, cameraRange)
+cameraValue = False
+
+ambientLight = THREE.AmbientLight.new(0xFFFFFF, 0.1)
+
+light = THREE.SpotLight.new(0xFFFFFF, 3)
+light.position.set(5, 5, 2)
+light.castShadow = True
+light.shadow.mapSize.width = 10000
+light.shadow.mapSize.height = light.shadow.mapSize.width
+light.penumbra = 0.5
+
+lightBack = THREE.PointLight.new(0x0FFFFF, 1)
+lightBack.position.set(0, -3, -1)
+
+scene.add(sceneGroup)
+scene.add(light)
+scene.add(lightBack)
+
+rectSize = 2
+intensity = 14
+rectLight = THREE.RectAreaLight.new(0x0FFFFF, intensity, rectSize, rectSize)
+rectLight.position.set(0, 0, 1)
+rectLight.lookAt(0, 0, 0)
+scene.add(rectLight)
+
+raycaster = THREE.Raycaster.new()
+uSpeed = 0.1
+
+time = 0.0003
+camera.lookAt(scene.position)
+
+
+async def main():
+    while True:
+        time = performance.now() * 0.0003
+        i = 0
+        while i < particularGroup.children.length:
+            newObject = particularGroup.children[i]
+            newObject.rotation.x += newObject.speedValue / 10
+            newObject.rotation.y += newObject.speedValue / 10
+            newObject.rotation.z += newObject.speedValue / 10
+            i += 1
+
+        i = 0
+        while i < modularGroup.children.length:
+            newCubes = modularGroup.children[i]
+            newCubes.rotation.x += 0.008
+            newCubes.rotation.y += 0.005
+            newCubes.rotation.z += 0.003
+
+            newCubes.position.x = (
+                Math.sin(time * newCubes.positionZ) * newCubes.positionY
+            )
+            newCubes.position.y = (
+                Math.cos(time * newCubes.positionX) * newCubes.positionZ
+            )
+            newCubes.position.z = (
+                Math.sin(time * newCubes.positionY) * newCubes.positionX
+            )
+            i += 1
+
+        particularGroup.rotation.y += 0.005
+
+        modularGroup.rotation.y -= ((mouse.x * 4) + modularGroup.rotation.y) * uSpeed
+        modularGroup.rotation.x -= ((-mouse.y * 4) + modularGroup.rotation.x) * uSpeed
+
+        renderer.render(scene, camera)
+        await asyncio.sleep(0.02)
+
+
+asyncio.ensure_future(main())
+
+
+async def run_code(event):
+    # runButton = document.querySelector("#runButton")
+    # codeEditor = document.querySelector("#codeEditor")
+
+    code_string = window.editor.getValue()  # Get the code from the editor
+    printouts = document.querySelector("#printouts")
+    printouts.innerHTML = ""
+
+    FUNC_NAME = "update_pixels"
+
+    try:
+        # Define a custom stream that captures the output
+        class CapturePrintouts:
+            def write(self, text):
+                printouts.innerHTML += text
+
+        # Redirect stdout to the custom stream
+        sys.stdout = CapturePrintouts()
+
+        # Create a separate namespace for the executed code
+        namespace = {}
+        # Execute the user-provided code
+        exec(code_string, namespace)
+
+        # Check if the 'update_pixels' function is defined in the user-provided code
+        if FUNC_NAME not in namespace:
+            printouts.innerHTML += f"Error:\n 'def {FUNC_NAME}():' function not found in the provided code."
+            return
+
+        # Get a reference to the 'update' function
+        update_pixels_func = namespace[FUNC_NAME]
+
+        # Call the 'update' function repeatedly
+        for i in range(10):
+            temp = str(update_pixels_func(i)) + "\n"
+            printouts.innerHTML += temp
+            await asyncio.sleep(0.5)
+    except:
+        printouts.innerText += traceback.format_exc()
+    finally:
+        # Restore the original stdout
+        sys.stdout = sys.__stdout__
